3f9c2c070fb36c88bb76229b1e3dfae77d7211d6
ibbiak <ibrahim.khaliq04@alumni.imperial.ac.uk>
2014-05-11 22:52:06 +0300

    Plot1

--- plot1.R
+++ plot1.R
@@ -9,5 +9,5 @@ data <- data[(data$Date == "1/2/2007") | (data$Date == "2/2/2007"),]
 
 ## Plot and write chart
 png(filename = "plot1.png", width = 480, height = 480, units = "px")
-hist(data$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red", xlim = c(0,6))
+hist(data$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
 dev.off()
